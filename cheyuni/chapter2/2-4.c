251157e034537050f379652ada70ec4bd27283e3
cheyuni <ok7217@gmail.com>
2013-01-20 10:22:59 +0900

    wp

--- cheyuni/chapter2/2-4.c
+++ cheyuni/chapter2/2-4.c
@@ -3,34 +3,27 @@
 /* Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes each character in s1 that matches any character in */
 /* the string s2. */
 
-/* squeeze: delete all c from s */
-/* void squeeze(char s[], char c[]){ */
-/*     int i, j, k; */
-/*     int index_array[strlen(c)]; */
-/*     int length = strlen(s); */
+void squeeze(char s1[], char s2[]){
+	int i, i2, j;
 
-/*     for(i = 0; i < strlen(c); i++){ */
-/* 	for(j = 0; j < strlen(s); j++){ */
-/* 	    int k = i; */
-/* 	    printf("%c", c[k]); */
-/* 	    printf("%c", c[j]);	     */
-/* 	    if(c[k++] == s[j]){ */
-/* 		/\* printf("error"); *\/ */
-/* 	    } */
-/* 	} */
-/* 	printf("\n"); */
-/*     } */
-
-    
-/* } */
+	for (i = j = 0; s1[i] != '\0'; i++) {
+		for (i2 = 0; s2[i2] != '\0'; i2++) {
+			if (s2[i2] == s1[i])
+				break;
+		}
+		if (s2[i2] == '\0')
+			s1[j++] = s1[i];
+	}
+	s1[j] = '\0';
+}
 
 int main(int argc, char *argv[]){
     char hex[10] = "0x329";
     char target[10] = "0x";
-    /* squeeze(hex, target); */
+    squeeze(hex, target);
 
-    printf("%d", hex);
-    printf("%d", target);    
+    printf("%s", hex);
+    /* printf("%d", target);     */
 
     return 0;
 }
